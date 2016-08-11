084c2ab7c283acdfe289333b66c9afa8925bff9d
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2016-08-11 17:45:04 +0200

    Chap 5-2: Se añade el historico de página visitadas a la página de login, y a la principal un enlace a la de login

--- js/views/appHeader.jsx
+++ js/views/appHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default React.createClass({
   render: function () {
     return (
-    	<header className="app-header">app header</header>
+    	<header className="app-header">app header
+    	<Link to="/login">Log in</Link>
+    	</header>
     );
   }
 });
--- js/views/login.jsx
+++ js/views/login.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { History } from 'react-router';
 
 export default React.createClass({
+  mixins: [History],
+  logIn: function(e)  {
+  	this.history.pushState('','/');
+  },
   render: function () {
     return (
     	<form className="login-form">login form</form>
